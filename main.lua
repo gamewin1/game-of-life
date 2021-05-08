8b18699b82608ab89cedf1c621dbc4e58783346f
Jesse Dufrene <jessecdufrene@gmail.com>
2021-05-08 03:16:08 -0500

    get working grid rendering

--- main.lua
+++ main.lua
@@ -1,20 +1,23 @@
 -- CONFIG
-local cellBorderSize = 1
-local cellSize = 8
--- local borderColor =
--- local aliveColor =
--- local deadColor =
+local cellBorderSize = 2
+local cellSize = 30
+-- {red, green, blue, alpha}
+local borderColor = {0.2, 0.2, 0.2, 1}
+local aliveColor = {0.9, 1, 0.9, 1}
+local deadColor = {0.1, 0, 0, 1}
+local cellsWide = 40
+local cellsTall = 25
 -- END CONFIG
 
 function love.load()
-    setWindow(80,50)
+    setWindow(cellsWide,cellsTall)
 end
 function love.update(dt)
 
 end
 
 function love.draw()
-
+    drawGrid()
 end
 
 ---Sets window size based on the grid size
@@ -26,4 +29,29 @@ function setWindow(w,h)
     end
     local windowWidth, windowHeight = getDimensionSize(w), getDimensionSize(h)
     love.window.setMode(windowWidth, windowHeight)
+end
+
+function drawGrid()
+    -- center of top left pixel is 0.5, 0.5
+    love.graphics.setColor(borderColor)
+    love.graphics.setLineWidth(cellBorderSize)
+    local centerOfLine = cellBorderSize*0.5
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    ---Given a cell count along a dimension, get each border coordinate
+    function getCoordinatesToDraw(cellCount)
+        local coordinates = {}
+        for i=0,cellCount do
+            local coordinate = ((cellBorderSize+cellSize)*i)+centerOfLine
+            coordinates[i+1] = coordinate
+        end
+        return coordinates
+    end
+    local xCoords = getCoordinatesToDraw(cellsWide)
+    local yCoords = getCoordinatesToDraw(cellsTall)
+    for k,v in pairs(xCoords) do
+        love.graphics.line(v, 0.5, v, windowHeight+0.5)
+    end
+    for k,v in pairs(yCoords) do
+        love.graphics.line(0.5, v, windowWidth+0.5, v)
+    end
 end
