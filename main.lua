250547b0c11692dbb42ff93cd0a5e4922275a304
Jesse Dufrene <jessecdufrene@gmail.com>
2021-05-08 02:15:37 -0500

    doing some basic setup, started on the config

---
+++ main.lua
@@ -0,0 +1,18 @@
+-- CONFIG
+local cellBorderSize = 1
+local cellSize = 8
+-- local borderColor =
+-- local aliveColor =
+-- local deadColor =
+-- END CONFIG
+
+function love.load()
+
+end
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
